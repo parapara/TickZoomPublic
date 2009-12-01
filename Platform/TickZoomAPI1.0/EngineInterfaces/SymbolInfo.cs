31fb99775b3b0012cc6b11e922120a6128a05b9d
Wayne Walter <wayne@tickzoom.com>
2009-11-30 19:00:17 -0500

    Added Intellisense documentation to SymbolInfo.

--- Platform/TickZoomAPI1.0/EngineInterfaces/SymbolInfo.cs
+++ Platform/TickZoomAPI1.0/EngineInterfaces/SymbolInfo.cs
@@ -34,34 +34,63 @@ namespace TickZoom.Api
 {
 	public interface SymbolInfo
 	{
+		/// <summary>
+		/// The actual text value of the symbol.
+		/// </summary>
 		string Symbol {
 			get;
 		}
 	
+		/// <summary>
+		/// The universal symbol ties different symbols together from different
+		/// data providers and brokers which actually represent the same instrument.
+		/// In that case, they will all have the same UnivesalSymbol.
+		/// </summary>
 		SymbolInfo UniversalSymbol {
 			get;
 		}
 	
+		/// <summary>
+		/// The binary identifier is a unique number assigned to every symbol
+		/// each time the system starts. Therefore this number can change any time
+		/// a symbol is added or removed from the dictionary upon the next time it
+		/// restarts. For that reason it is useless to use this in user strategy code.
+		/// It's purpose internally to TickZoom is to greatly increase performance
+		/// by refering to a native number rather than a string to represet symbols.
+		/// </summary>
 		ulong BinaryIdentifier {
 			get;
 		}
 		
-//		[Obsolete("Please use the symbol dictionary to set this value.")]
+		/// <summary>
+		/// The time of day that the primary session for this symbol starts.
+		/// </summary>
 		Elapsed SessionStart {
 			get;
 			set;
 		}
 		
-//		[Obsolete("Please use the symbol dictionary to set this value.")]
+		/// <summary>
+		/// The time of day that the primary session for this symbol ends.
+		/// </summary>
 		Elapsed SessionEnd {
 			get;
 			set;
 		}
 		
+		/// <summary>
+		/// Returns a fractional value for the minimum price move for the symbol.
+		/// For example: And U.S. stock's minimum tick will be 0.01
+		/// </summary>
 		double MinimumTick {
 			get;
 		}
 		
+		/// <summary>
+		/// The currency value of a full point of a symbol in its denominated currency.
+		/// You can simply multiple FullPointValue by the any price of the symbol to get the
+		/// full currency value of that price.
+		/// </summary>
 		double FullPointValue {
 			get;
 		}
@@ -73,15 +102,30 @@ namespace TickZoom.Api
 			get;
 		}
 		
+		/// <summary>
+		/// Sets the divisor for lots when collecting Depth of Market data for
+		/// every tick. This helps to standardize across different markets which 
+		/// use different sizes of orders to indicate 1 full lot. So for stocks this
+		/// should ordinarily be set to 100 whereas for Forex the standard lot size
+		/// is 10000.
+		/// </summary>
 		int Level2LotSize {
 			get;
 		}
 		
+		/// <summary>
+		/// This increment in price between each level of Depth of Market
+		/// which you wish to collect.
+		/// </summary>
 		double Level2Increment {
 			get;
 		}
 		
-		int Level2LotSizeMinimum {
+ 		/// <summary>
+ 		/// Eliminate reporting of orders on Level II less
+		/// than a the lot size minimum.
+ 		/// </summary>
+ 		int Level2LotSizeMinimum {
 			get;
 		}
 	}
