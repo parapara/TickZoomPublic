75ecbd5333cf44e7a95404e7856eff095c974d31
Wayne Walter <wayne@tickzoom.com>
2009-12-06 01:57:27 -0500

    Added user manual documentation into auto build via Doxygen.

---
+++ Platform/TickZoomAPI1.0/Docs/Pages.cs
@@ -0,0 +1,45 @@
+﻿#region Copyright
+/*
+ * Copyright 2008 M. Wayne Walter
+ * Software: TickZoom Trading Platform
+ * User: Wayne Walter
+ * 
+ * You can use and modify this software under the terms of the
+ * TickZOOM General Public License Version 1.0 or (at your option)
+ * any later version.
+ * 
+ * Businesses are restricted to 30 days of use.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * TickZOOM General Public License for more details.
+ *
+ * You should have received a copy of the TickZOOM General Public
+ * License along with this program.  If not, see
+ * 
+ * 
+ *
+ * User: Wayne Walter
+ * Date: 12/6/2009
+ * Time: 1:48 AM
+ * <http://www.tickzoom.org/wiki/Licenses>.
+ */
+#endregion
+
+	/// 
+	/// @mainpage Main Page of TickZoomAPI1.0
+	/// 
+	/// @section intro_section Introduction
+	/// The TickZoomAPI is a project made up of Interfaces, Enums,
+	/// Exceptions, and Factories that tie all the assemblies of the
+	/// user strategies, the TickZoom Platform, Data Engine and Warehouse
+	/// together.
+	/// 
+	/// 
+	/// 
+	/// 
+	/// 
+	/// @section engine_section Engine Interfaces
+	/// These interfaces are implemented by the TickZoom Data Engine.
+	/// 
