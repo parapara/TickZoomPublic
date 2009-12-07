577467940fe130e572a9b6c161720a8e1832a87f
AutoBuild <wayne@tickzoom.com>
2009-12-07 04:23:57 -0500

    Build version 0.5.36.226.

--- Platform/TickZoomGui/Properties/AssemblyInfo.cs
+++ Platform/TickZoomGui/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.5.31.1141")]
+[assembly: AssemblyVersion("0.5.36.226")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
