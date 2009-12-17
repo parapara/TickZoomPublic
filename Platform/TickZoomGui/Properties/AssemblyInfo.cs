b74a0a5f2b6b3bf6f1c3a28a4351ae02a9aee153
Wayne Walter <wayne@tickzoom.com>
2009-12-17 12:05:14 -0500

    Build version 0.6.46.671.

--- Platform/TickZoomGui/Properties/AssemblyInfo.cs
+++ Platform/TickZoomGui/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.5.37.970")]
+[assembly: AssemblyVersion("0.6.46.671")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
--- Platform/TickZoomGui2/Properties/AssemblyInfo.cs
+++ Platform/TickZoomGui2/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.5.37.970")]
+[assembly: AssemblyVersion("0.6.46.671")]
 //[assembly: AssemblyFileVersion("2.0.0.0")]
